f80e3759133a1916061fcd9d70ee8530dbe13540
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-09-05 21:37:25 +0100

    Bug: Didnt find a fix for the hot reload

--- app/(tabs)/home.js
+++ app/(tabs)/home.js
@@ -1,27 +1,14 @@
-import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const home = () => {
   return (
-    <SafeAreaView
-      style={[
-        { flex: 1 },
-        Platform.OS === "android" ? { marginTop: 24 } : null,
-        styles.container,
-      ]}
-    >
-      <View>
-        <Text>Home</Text>
-      </View>
-    </SafeAreaView>
+    <View>
+      <Text>home</Text>
+    </View>
   );
 };
 
 export default home;
 
-const styles = StyleSheet.create({
-  container: {
-    // backgroundColor: "black"
-    padding: 30,
-  },
-});
+const styles = StyleSheet.create({});
