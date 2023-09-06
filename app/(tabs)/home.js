cc90eb41398fb2e5d253e4f525c82be0f7679512
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-09-06 19:59:38 +0100

    feat: Added the text and search bar

--- app/(tabs)/home.js
+++ app/(tabs)/home.js
@@ -5,6 +5,7 @@ import {
   View,
   Platform,
   TouchableWithoutFeedback,
+  TextInput,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 // import { generalFood } from "../food";
@@ -28,12 +29,16 @@ const Home = () => {
   // console.log({ products });
   return (
     <SafeAreaView
-      style={{
-        marginTop: Platform.OS === "android" ? 30 : 0,
-        paddingLeft: 30,
-        paddingRight: 30,
-      }}
+      style={[
+        {
+          marginTop: Platform.OS === "android" ? 30 : 0,
+          paddingLeft: 30,
+          paddingRight: 30,
+        },
+        styles.container,
+      ]}
     >
+      {/* Icon */}
       <View style={styles.icon}>
         <TouchableWithoutFeedback>
           <Image source={require("../../assets/images/hamburger.png")} />
@@ -42,6 +47,21 @@ const Home = () => {
           <Image source={require("../../assets/images/shopping-cart.png")} />
         </TouchableWithoutFeedback>
       </View>
+
+      {/* Text */}
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Delicious</Text>
+        <Text style={styles.text}>food for you</Text>
+      </View>
+
+      {/* Search Bar */}
+
+      <View style={styles.inputContainer}>
+        <View>
+          <Image source={require("../../assets/images/search.png")} />
+        </View>
+        <TextInput style={styles.input} />
+      </View>
     </SafeAreaView>
   );
 };
@@ -49,9 +69,37 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#F2F2F2",
+  },
   icon: {
     // backgroundColor: "black",
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  text: {
+    width: "185px",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  textContainer: {
+    marginTop: 40,
+  },
+  inputContainer: {
+    backgroundColor: "grey",
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: 50,
+    padding: 10,
+    width: "80%",
+    marginTop: 20,
+    justifyContent: "center",
+  },
+  input: {
+    // backgroundColor: "black",
+    // borderWidth: 1,
+    paddingLeft: 10,
+    width: "90%",
+    marginLeft: 5,
   },
 });
