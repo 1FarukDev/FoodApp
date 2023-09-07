d56daf56d51cd15de12619204179e59705bec27b
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-09-07 13:53:36 +0100

    feat: Beaan able to switch screen

---
+++ components/foods.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const foods = () => {
+  return (
+    <View>
+      <Text>foods</Text>
+    </View>
+  );
+};
+
+export default foods;
+
+const styles = StyleSheet.create({});
---
+++ components/snacks.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const snacks = () => {
+  return (
+    <View>
+      <Text>snacks</Text>
+    </View>
+  );
+};
+
+export default snacks;
+
+const styles = StyleSheet.create({});
