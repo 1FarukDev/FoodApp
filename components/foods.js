f7549c2d0a6306997f95a3254425df016b560466
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-09-08 07:31:20 +0100

    feat: styles the food component

--- components/foods.js
+++ components/foods.js
@@ -1,30 +1,38 @@
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import React from "react";
 import FoodItem from "./foodItem";
-import foodItem from "./foodItem";
+import food1 from "../assets/images/food1.png";
+import food2 from "../assets/images/food2.png";
+import food3 from "../assets/images/food3.png";
 const foods = () => {
   const food = [
     {
       key: 0,
-      name: "eba",
-      price: "1200",
+      name: "Veggie Tomato Mix",
+      price: "N1,200",
+      image: food1,
     },
     {
       key: 1,
-      name: "rice",
-      price: "1200",
+      name: "Egg and cucumber",
+      price: "N1,200",
+      image: food2,
     },
     {
       key: 2,
-      name: "iyan",
-      price: "1200",
+      name: "Moi-moi and ekpa",
+      price: "N1,200",
+      image: food3,
     },
   ];
   return (
-    <View>
+    <View style={styles.foodList}>
       <FlatList
         data={food}
         renderItem={({ item }) => <FoodItem item={item} />}
+        horizontal={true}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
       />
     </View>
   );
@@ -32,4 +40,11 @@ const foods = () => {
 
 export default foods;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  foodList: {
+    flexDirection: "row",
+    alignItems: "center",
+    // borderWidth: 2,
+    // backgroundColor: "black",
+  },
+});
